a51da6ca1576dffe761a9b5e90589eda968c5d8a
kosior3x <67843228+kosior3x@users.noreply.github.com>
2026-01-04 12:37:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    # Tu Alex serwuje Twoje demo Vis-Sol
+def index():
+    # Serwuje Twoje profesjonalne demo Vis-Sol
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # Railway dynamicznie przydziela port, musimy go pobrać
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
